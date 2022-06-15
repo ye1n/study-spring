82b4e81cd6ccdd390db86ec50e0afdacb1fecf16
우예인 <uyeinyein@gmail.com>
2022-06-15 15:19:59 +0900

    AOP

--- hello-spring/src/main/java/spring/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/spring/hellospring/SpringConfig.java
@@ -1,4 +1,6 @@
 package spring.hellospring;
+import org.springframework.beans.factory.annotation.Autowired;
+import spring.hellospring.aop.TimeTraceAop;
 import spring.hellospring.repository.*;
 import spring.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
@@ -11,12 +13,19 @@ public class SpringConfig {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public SpringConfig(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository);
     }
+
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
 
 }
